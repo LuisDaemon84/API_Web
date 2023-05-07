0cbbab882514c5cbd68cf63f1a240cfff6a45b77
LuisDaemon84 <siulzitro01@hotmail.com>
2023-05-07 17:46:39 -0500

    Dto y Autommaper

---
+++ API_King/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using API_King.Modelos;
+using API_King.Modelos.Dto;
+using AutoMapper;
+using System.Runtime.InteropServices;
+
+namespace API_King
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDto>();
+            CreateMap<VillaDto, Villa>();
+
+            CreateMap<Villa, VillaCreateDto>().ReverseMap();
+            CreateMap<Villa, VillaCreateDto>().ReverseMap();
+        }
+    }
+}
